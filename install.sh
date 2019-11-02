fcf8c2b5f2b5f3d9f1dddd71d48326bd592524cc
xiejunyi <xie.junyi@outlook.com>
2019-11-02 21:05:20 +0800

    init

---
+++ install.sh
@@ -0,0 +1,5 @@
+CurDir=`pwd`
+ScriptPath="$CurDir/xiejunyi.sh"
+echo "install scpript: $ScriptPath in ~/.zshrc, ~/.bashrc"
+echo "source $ScriptPath" >> ~/.bashrc
+echo "source $ScriptPath" >> ~/.zshrc
---
+++ xiejunyi.sh
@@ -0,0 +1,16 @@
+# path
+alias p='pwd | pbcopy'
+
+# git
+alias gs='git status'
+alias gc='git checkout'
+alias ga='git add .'
+alias gpl='git pull'
+alias gb='git branch'
+alias gtc='git add .;git commit -m "tem";'
+
+
+# shadowsocks-XNG proxy
+export http_proxy=socks5://127.0.0.1:1086 # 配置http访问的
+export https_proxy=socks5://127.0.0.1:1086 # 配置https
+export all_proxy=socks5://127.0.0.1:1086 # 配置http和https访问
